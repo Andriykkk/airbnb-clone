d20dc6fb079ddede188520662e552d2ef0929161
Andriykkk <andriykostenkostd@gmail.com>
2024-06-05 18:04:14 +0300

    resolve dynamic server usage problem

--- app/page.tsx
+++ app/page.tsx
@@ -12,34 +12,38 @@ interface HomeProps {
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
+  try {
+    const listings = await getListings(searchParams);
+    const currentUser = await getCurrentUser();
 
-  if (listings.length === 0) {
+    if (listings.length === 0) {
+      return (
+        <ClientOnly>
+          <EmptyState showReset />
+        </ClientOnly>
+      );
+    }
+
     return (
       <ClientOnly>
-        <EmptyState showReset />
+        <Container>
+          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+            {listings.map((listing: any) => {
+              return (
+                <ListingCard
+                  currentUser={currentUser}
+                  key={listing.id}
+                  data={listing}
+                />
+              );
+            })}
+          </div>
+        </Container>
       </ClientOnly>
     );
+  } catch (error) {
+    return;
   }
-
-  return (
-    <ClientOnly>
-      <Container>
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          {listings.map((listing: any) => {
-            return (
-              <ListingCard
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-              />
-            );
-          })}
-        </div>
-      </Container>
-    </ClientOnly>
-  );
 };
 
 export default Home;
